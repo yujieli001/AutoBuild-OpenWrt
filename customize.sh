ac20bbd9624e8a59555f3227e25661391d6a37a3
yujieli <yujieli001@live.cn>
2024-01-31 11:38:49 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,7 +6,7 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/10.28.73.401/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.28.73.40/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 #sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
